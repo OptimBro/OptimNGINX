063166c740b8fd094f0c9d1ab10d58017464457b
Rahul <52219366+OptimBro@users.noreply.github.com>
2019-07-02 00:28:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Define versions
+OPTIM_NGINX_VER=18.0
+NGINX_MAINLINE_VER=1.17.0
+NGINX_STABLE_VER=1.16.0
+LIBRESSL_VER=2.9.0
+OPENSSL_VER=1.1.1a
+NPS_VER=1.13.35.2
+HEADERMOD_VER=0.33
+LIBMAXMINDDB_VER=1.3.2
+GEOIP2_VER=3.2
+HTTP_REDIS_VER=0.3.9
+PCRE_NGINX_VER=8.42
+ZLIB_NGINX_VER=1.2.11 
+
 echo "Starting script"
 sleep 2
 wget -qO /opt/dep.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/dep.sh
