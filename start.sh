62f25999d596cc59cb4492a25995016559f35ae6
Rahul <52219366+OptimBro@users.noreply.github.com>
2019-07-02 12:48:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,56 +1,49 @@
 #!/bin/bash
 
 # Define versions
-OPTIM_NGINX_VER=18.0
-NGINX_VER=1.17.0
-LIBRESSL_VER=2.9.0
-OPENSSL_VER=1.1.1a
-NPS_VER=1.13.35.2
-HEADERMOD_VER=0.33
-LIBMAXMINDDB_VER=1.3.2
-GEOIP2_VER=3.2
-HTTP_REDIS_VER=0.3.9
-PCRE_NGINX_VER=8.42
-ZLIB_NGINX_VER=1.2.11
+OPTIM_NGINX_VER="18.0"
+NGINX_VER="1.17.0"
+LIBRESSL_VER="2.9.0"
+OPENSSL_VER="1.1.1a"
+NPS_VER="1.13.35.2"
+HEADERMOD_VER="0.33"
+LIBMAXMINDDB_VER="1.3.2"
+GEOIP2_VER="3.2"
+HTTP_REDIS_VER="0.3.9"
+PCRE_NGINX_VER="8.42"
+ZLIB_NGINX_VER="1.2.11"
 
-rm -rf /opt/dep.sh
-rm -rf /opt/compile.sh
-rm -rf /opt/install.sh
+export OPTIM_NGINX_VER
+export NGINX_VER
+export LIBRESSL_VER
+export OPENSSL_VER
+export NPS_VER
+export HEADERMOD_VER
+export LIBMAXMINDDB_VER
+export GEOIP2_VER
+export HTTP_REDIS_VER
+export PCRE_NGINX_VER
+export ZLIB_NGINX_VER
 
-echo "Starting script"
-sleep 2
-wget -qO /opt/dep.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/dep.sh
+rm -rf /opt/optimengine/dep.sh
+rm -rf /opt/optimengine/compile.sh
+rm -rf /opt/optimengine/install.sh
 
-chmod +x /opt/dep.sh
+wget -qO /opt/optimengine/dep.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/dep.sh
+chmod +x /opt/optimengine/dep.sh
 
-bash /opt/dep.sh
+wget -qO /opt/optimengine/compile.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/compile.sh
+chmod +x /opt/optimengine/compile.sh
 
-echo -ne '#####                     (33%)\r'
-sleep 3
+wget -qO /opt/optimengine/install.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/install.sh
+chmod +x /opt/optimengine/install.sh
 
-echo "Compiling..."
-sleep 2
-wget -qO /opt/compile.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/compile.sh
+cd /opt/optimengine/
 
-chmod +x /opt/compile.sh
+./dep.sh
+./compile.sh
+./install.sh
 
-bash /opt/compile.sh
 
-echo -ne '#############             (66%)\r'
-sleep 3
-
-echo "Installing..."
-sleep 2
-
-wget -qO /opt/install.sh --timeout=15 https://raw.githubusercontent.com/OptimBro/OptimNGINX/master/install.sh
-
-chmod +x /opt/install.sh
-
-bash /opt/install.sh
-
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-
-sleep 3
 
 echo "Nginx is ready"
